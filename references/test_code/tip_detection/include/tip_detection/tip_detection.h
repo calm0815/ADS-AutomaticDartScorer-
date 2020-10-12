5608fbc874b5662a0ab858af9de96eb54504f761
calm0815 <calm0815@example.com>
2020-10-13 08:10:52 +0900

    update class function

--- references/test_code/tip_detection/include/tip_detection/tip_detection.h
+++ references/test_code/tip_detection/include/tip_detection/tip_detection.h
@@ -6,10 +6,35 @@
 class TipDetection
 {
 private:
-    /* data */
+    bool isDebug_;
+
+    // image
+    cv::Mat src_;
+    cv::Mat tip_mask_;
+
+    // color range
+    cv::Scalar yellow_tip_color_range_min_;
+    cv::Scalar yellow_tip_color_range_max_;
+    cv::Scalar green_tip_color_range_min_;
+    cv::Scalar green_tip_color_range_max_;
+
+    // tip coordinate
+    cv::Point tip_position_;
+
+private:
+    cv::Mat extractColor(cv::Mat image, cv::Scalar range_min, cv::Scalar range_max, int opening, int closing);
+    cv::Point estimateTipPosition(cv::Mat mask, std::string direction);
+
 public:
-    TipDetection();
+    TipDetection(const cv::Mat src, const bool isDebug);
     ~TipDetection();
+
+    void applyDetection();
+
+    void setYellowTipRange(cv::Scalar min, cv::Scalar max);
+    void setGreenTipRange(cv::Scalar min, cv::Scalar max);
+    cv::Mat getTipMask();
+    cv::Point getTipPosition();
 };
 
 #endif  // TIP_DETECTION_H_
--- references/test_code/tip_detection/src/main.cpp
+++ references/test_code/tip_detection/src/main.cpp
@@ -1,22 +1,15 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
 
 #include "tip_detection/tip_detection.h"
 
 // yellow
-const int B_MAX = 70;
-const int B_MIN = 0;
-const int G_MAX = 200;
-const int G_MIN = 100;
-const int R_MAX = 210;
-const int R_MIN = 100;
+cv::Scalar yellow_min = cv::Scalar(0, 100, 100);
+cv::Scalar yellow_max = cv::Scalar(70, 200, 210);
 
 int main(int argc, char *argv[])
 {
     cv::Mat image;
-    cv::Mat mask, result;
-
-    TipDetection tip_detection();
+    cv::Mat result;
 
     image = cv::imread("../../../../resources/yellow.jpeg");
 	if (image.empty())
@@ -26,43 +19,13 @@ int main(int argc, char *argv[])
 	}
     cv::resize(image, image, cv::Size(), 0.3, 0.3);
 
-	// inRangeを用いて色抽出フィルタリング
-	cv::Scalar s_min = cv::Scalar(B_MIN, G_MIN, R_MIN);
-	cv::Scalar s_max = cv::Scalar(B_MAX, G_MAX, R_MAX);
-	cv::inRange(image, s_min, s_max, mask);
+    TipDetection tip_detection(image, true);
 
-    // マスク画像のOpening処理
-    cv::morphologyEx(mask, mask, cv::MORPH_OPEN, cv::Mat());
-    cv::threshold(mask, mask, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU); // 2値化（閾値を自動で設定）
+    tip_detection.setYellowTipRange(yellow_min, yellow_max);
+    tip_detection.applyDetection();
 
-    std::vector<int> point(mask.rows, mask.cols);
-
-    for (int i=0; i<mask.rows; i++)
-    {
-        for (int j=0; j<mask.cols; j++)
-        {
-            if (static_cast<int>(mask.at<unsigned char>(i, j)) != 0)
-            {
-                // std::cout << "(row, col) = " << i << ", " << j << std::endl;
-                if (j < point[1])
-                {
-                    point[0] = i;
-                    point[1] = j;
-                }
-            }
-        }
-    }
-
-    std::cout << "The coordinate of the tip of the dart is (" << point[0] << ", " << point[1] << ")" << std::endl;
-
-    // マーキング
-    cv::circle(image, cv::Point(point[1],point[0]), 20, cv::Scalar(0,255,0), 2);
-
-    // 表示
-    cv::namedWindow("mask image", cv::WINDOW_NORMAL);
-    cv::imshow("mask image", mask);
-    cv::namedWindow("result image", cv::WINDOW_NORMAL);
-    cv::imshow("result image", image);
+    cv::Mat mask = tip_detection.getTipMask();
+    cv::Point position = tip_detection.getTipPosition();
 
     cv::waitKey(0);
     return 0;
