ef86a6c7c163d05d0ecd9f244aaa3f7a24387757
calm0815 <calm0815@example.com>
2020-10-13 06:38:21 +0900

    fix path & apply debug

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,22 @@
 int main(int argc, char *argv[])
 {
     // Load left image
-    cv::Mat left_image = cv::imread("../../resources/capture_l_plane.png");
+    cv::Mat left_image = cv::imread("../resources/capture_l_plane.png");
 	if (left_image.empty())
     {
 		std::cerr << "Not found input image ..." << std::endl;
 		return -1;
 	}
     // Load right image
-    cv::Mat right_image = cv::imread("../../resources/capture_r_plane.png");
+    cv::Mat right_image = cv::imread("../resources/capture_r_plane.png");
 	if (right_image.empty())
     {
 		std::cerr << "Not found input image ..." << std::endl;
 		return -1;
 	}
 
-    BoardDetection left_board_detection(left_image);
-    BoardDetection right_board_detection(right_image);
-
     // Load front view image
-    cv::Mat front_image = cv::imread("../../resources/front_view.jpg");
+    cv::Mat front_image = cv::imread("../resources/front_view.jpg");
 	if (front_image.empty())
     {
 		std::cerr << "Not found front view image ..." << std::endl;
@@ -34,6 +31,9 @@ int main(int argc, char *argv[])
 	}
     cv::blur(front_image, front_image, cv::Size(2, 2));
 
+
+    BoardDetection left_board_detection(left_image, false);
+    BoardDetection right_board_detection(right_image, false);
 
     left_board_detection.setBoardColorRange(cv::Scalar(0, 0, 0), cv::Scalar(100, 90, 60));
     left_board_detection.setBoardEllipseThreshold(1500, 2000);
@@ -47,15 +47,10 @@ int main(int argc, char *argv[])
     right_board_detection.setReferenceImage(front_image);
     right_board_detection.applyDetection();
 
-
-    cv::namedWindow("input left image", cv::WINDOW_AUTOSIZE|cv::WINDOW_FREERATIO);
-    cv::imshow("input left image", left_image);
-    cv::namedWindow("input right image", cv::WINDOW_AUTOSIZE|cv::WINDOW_FREERATIO);
-    cv::imshow("input right image", right_image);
-    cv::namedWindow("the result of geometric transformation [left image]", cv::WINDOW_AUTOSIZE|cv::WINDOW_FREERATIO);
-    cv::imshow("the result of geometric transformation [left image]", left_board_detection.getTransformedImage());
-    cv::namedWindow("the result of geometric transformation [right image]", cv::WINDOW_AUTOSIZE|cv::WINDOW_FREERATIO);
-    cv::imshow("the result of geometric transformation [right image]", right_board_detection.getTransformedImage());
+    cv::Mat left_transform_matrix = left_board_detection.getTransformMatrix();
+    cv::Mat left_front_view = left_board_detection.getTransformedImage();
+    cv::Mat right_transform_matrix = right_board_detection.getTransformMatrix();
+    cv::Mat right_front_view = right_board_detection.getTransformedImage();
 
 
     cv::waitKey(0);
