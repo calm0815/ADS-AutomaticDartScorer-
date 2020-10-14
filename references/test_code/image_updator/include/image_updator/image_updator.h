fda5aee5c05c247a713cae03e0d81b98b39f4c42
calm0815 <calm0815@example.com>
2020-10-14 19:33:51 +0900

    image_updator done !

--- references/test_code/image_updator/include/image_updator/image_updator.h
+++ references/test_code/image_updator/include/image_updator/image_updator.h
@@ -6,14 +6,33 @@
 class ImageUpdator
 {
 private:
-    /* data */
+    bool isDebug_;
+
+    cv::Mat frame_;
+    cv::Mat diff_;
+    cv::Mat segm_;
+    cv::Mat next_sequence_;
+
+    cv::Ptr<cv::BackgroundSubtractor> bgfs_;
+
+    bool flag_;
+    bool isUpdated_;
+    int counter_;
+    int nonzero_threshold_ = 1500;
 
 private:
 
 
 public:
-    ImageUpdator(/* args */);
+    ImageUpdator(const bool isDebug);
     ~ImageUpdator();
+
+    void updateFrame(cv::Mat image);
+    cv::Mat getFrame();
+    cv::Mat getDiffFrame();
+    cv::Mat getNextSequenceImage();
+    bool getUpdateFlag();
+    void setFlag(const bool flag);
 };
 
 #endif
